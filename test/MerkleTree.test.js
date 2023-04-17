a6753c058c2cd394dd6e2897fe4086e55814bf19
sherry <sherry@ebridge.exchange>
2023-04-17 20:27:29 +0800

    feat: add merkletree unit test.

--- test/MerkleTree.test.js
+++ test/MerkleTree.test.js
@@ -365,6 +365,36 @@ describe("MerkleTree", function () {
                 expect(lastLeafIndex).to.equal(nodeToAdd - 1);
 
             })
+            it("Should record nodes with one tree ", async function () {
+
+                const { merkleTree, owner, regimentId } = await loadFixture(deployMerkleTreeFixture);
+                var pathLength = 3;
+                var treeLeaveCount = 2 * pathLength;
+                var nodeToAdd = 7
+                var tx = await merkleTree.createSpace(regimentId, pathLength);
+                await tx.wait();
+                var spaceIdList = await merkleTree.getRegimentSpaceIdListMap(regimentId);
+                var actualSpaceId = spaceIdList[0];
+
+                var { leavesNode } = createLeavesNode(nodeToAdd);
+                await merkleTree.recordMerkleTree(actualSpaceId, [leavesNode[0]]);
+                await merkleTree.recordMerkleTree(actualSpaceId, [leavesNode[1]]);
+                await merkleTree.recordMerkleTree(actualSpaceId, [leavesNode[2]]);
+                await merkleTree.recordMerkleTree(actualSpaceId, [leavesNode[3]]);
+                await merkleTree.recordMerkleTree(actualSpaceId, [leavesNode[4]]);
+                await merkleTree.recordMerkleTree(actualSpaceId, [leavesNode[5]]);
+                await merkleTree.recordMerkleTree(actualSpaceId, [leavesNode[6]]);
+
+                var remainLeafCountAfter = await merkleTree.getRemainLeafCount(actualSpaceId);
+                expect(remainLeafCountAfter).to.equal(1);
+
+                var treeCount = await merkleTree.getFullTreeCount(actualSpaceId);
+                expect(treeCount).to.equal(0);
+
+                var lastLeafIndex = await merkleTree.getLastLeafIndex(actualSpaceId);
+                expect(lastLeafIndex).to.equal(nodeToAdd - 1);
+
+            })
 
         });
         describe("proof test", function () {
