105900c84bfa2179e45bde99d3ac735efaf5c062
sherry <sherry@ebridge.exchange>
2025-05-08 16:20:54 +0800

    feat:update limit event.

--- contracts/LimiterImplementation.sol
+++ contracts/LimiterImplementation.sol
@@ -7,158 +7,163 @@ import {DailyLimiter} from './libraries/DailyLimiter.sol';
 import '@openzeppelin/contracts/utils/math/SafeMath.sol';
 
 contract LimiterImplementation is ProxyStorage {
-  using DailyLimiter for DailyLimiter.DailyLimitTokenInfo;
-  using RateLimiter for RateLimiter.TokenBucket;
-  using SafeMath for uint256;
+    using DailyLimiter for DailyLimiter.DailyLimitTokenInfo;
+    using RateLimiter for RateLimiter.TokenBucket;
+    using SafeMath for uint256;
 
-  // key: tokenKey / swapId
-  mapping(bytes32 => RateLimiter.TokenBucket) private tokenBucket;
-  mapping(bytes32 => DailyLimiter.DailyLimitTokenInfo) private dailyLimit;
-  address public admin;
-  address public bridgeIn;
-  address public bridgeOut;
+    // key: tokenKey / swapId
+    mapping(bytes32 => RateLimiter.TokenBucket) private tokenBucket;
+    mapping(bytes32 => DailyLimiter.DailyLimitTokenInfo) private dailyLimit;
+    address public admin;
+    address public bridgeIn;
+    address public bridgeOut;
 
-  modifier onlyAdmin() {
-    require(msg.sender == admin, 'no permission');
-    _;
-  }
-  modifier onlyBridge() {
-    require(msg.sender == bridgeIn || msg.sender == bridgeOut, 'no permission');
-    _;
-  }
+    event TokenRateLimitConsumed(bytes32 bucketId, address tokenAddress, uint256 amount);
+    event TokenDailyLimitConsumed(bytes32 dailyLimitId, address tokenAddress, uint256 amount);
 
-  function changeAdmin(address _admin) external onlyAdmin{
-    require(_admin != address(0), "invalid input");
-    admin = _admin;
-  }
-
-  function initialize(address _bridgeIn, address _bridgeOut, address _admin) external onlyOwner {
-    require(bridgeIn == address(0), 'already initialized');
-    bridgeIn = _bridgeIn;
-    bridgeOut = _bridgeOut;
-    admin = _admin;
-  }
-
-  function consumeDailyLimit(
-    bytes32 dailyLimitId,
-    address tokenAddress,
-    uint256 amount
-  ) external onlyBridge {
-    dailyLimit[dailyLimitId]._consume(tokenAddress, amount);
-  }
-
-  function consumeTokenBucket(
-    bytes32 bucketId,
-    address tokenAddress,
-    uint256 amount
-  ) external onlyBridge {
-    tokenBucket[bucketId]._consume(tokenAddress, amount);
-  }
-
-  function setDailyLimit(
-    DailyLimiter.DailyLimitConfig[] memory dailyLimitConfigs
-  ) external onlyAdmin {
-    for (uint i = 0; i < dailyLimitConfigs.length; i++) {
-      DailyLimiter.DailyLimitConfig memory dailyLimitConfig = dailyLimitConfigs[i];
-      dailyLimit[dailyLimitConfig.dailyLimitId]._setDailyLimit(dailyLimitConfig);
+    modifier onlyAdmin() {
+        require(msg.sender == admin, 'no permission');
+        _;
     }
-  }
+    modifier onlyBridge() {
+        require(msg.sender == bridgeIn || msg.sender == bridgeOut, 'no permission');
+        _;
+    }
 
-  function getReceiptDailyLimit(
-    address token,
-    string memory targetChainId
-  ) public view returns (DailyLimiter.DailyLimitTokenInfo memory) {
-    bytes32 dailyLimitId = _generateTokenKey(token, targetChainId);
-    return dailyLimit[dailyLimitId]._currentDailyLimit();
-  }
+    function changeAdmin(address _admin) external onlyAdmin {
+        require(_admin != address(0), "invalid input");
+        admin = _admin;
+    }
 
-  function getSwapDailyLimit(
-    bytes32 swapId
-  ) public view returns (DailyLimiter.DailyLimitTokenInfo memory) {
+    function initialize(address _bridgeIn, address _bridgeOut, address _admin) external onlyOwner {
+        require(bridgeIn == address(0), 'already initialized');
+        bridgeIn = _bridgeIn;
+        bridgeOut = _bridgeOut;
+        admin = _admin;
+    }
+
+    function consumeDailyLimit(
+        bytes32 dailyLimitId,
+        address tokenAddress,
+        uint256 amount
+    ) external onlyBridge {
+        dailyLimit[dailyLimitId]._consume(tokenAddress, amount);
+        emit TokenDailyLimitConsumed(dailyLimitId, tokenAddress, amount);
+    }
+
+    function consumeTokenBucket(
+        bytes32 bucketId,
+        address tokenAddress,
+        uint256 amount
+    ) external onlyBridge {
+        tokenBucket[bucketId]._consume(tokenAddress, amount);
+        emit TokenRateLimitConsumed(bucketId,tokenAddress, amount);
+    }
+
+    function setDailyLimit(
+        DailyLimiter.DailyLimitConfig[] memory dailyLimitConfigs
+    ) external onlyAdmin {
+        for (uint i = 0; i < dailyLimitConfigs.length; i++) {
+            DailyLimiter.DailyLimitConfig memory dailyLimitConfig = dailyLimitConfigs[i];
+            dailyLimit[dailyLimitConfig.dailyLimitId]._setDailyLimit(dailyLimitConfig);
+        }
+    }
+
+    function getReceiptDailyLimit(
+        address token,
+        string memory targetChainId
+    ) public view returns (DailyLimiter.DailyLimitTokenInfo memory) {
+        bytes32 dailyLimitId = _generateTokenKey(token, targetChainId);
+        return dailyLimit[dailyLimitId]._currentDailyLimit();
+    }
+
+    function getSwapDailyLimit(
+        bytes32 swapId
+    ) public view returns (DailyLimiter.DailyLimitTokenInfo memory) {
         return dailyLimit[swapId]._currentDailyLimit();
-  }
-
-  function setTokenBucketConfig(RateLimiter.TokenBucketConfig[] memory configs) external onlyAdmin {
-    for (uint i = 0; i < configs.length; i++) {
-      RateLimiter.TokenBucketConfig memory config = configs[i];
-      tokenBucket[config.bucketId]._configTokenBucket(configs[i]);
     }
-  }
 
-  function getCurrentReceiptTokenBucketState(
-    address token,
-    string memory targetChainId
-  ) public view returns (RateLimiter.TokenBucket memory) {
-    bytes32 bucketId = _generateTokenKey(token, targetChainId);
-    return tokenBucket[bucketId]._currentTokenBucketState();
-  }
-
-  function getCurrentReceiptTokenBucketStates(
-    address[] memory tokens,
-    string[] memory targetChainIds
-  ) public view returns (RateLimiter.TokenBucket[] memory _tokenBuckets) {
-    _tokenBuckets = new RateLimiter.TokenBucket[](tokens.length);
-    for (uint i = 0; i < tokens.length; i++) {
-      bytes32 bucketId = _generateTokenKey(tokens[i], targetChainIds[i]);
-      _tokenBuckets[i] = tokenBucket[bucketId]._currentTokenBucketState();
+    function setTokenBucketConfig(RateLimiter.TokenBucketConfig[] memory configs) external onlyAdmin {
+        for (uint i = 0; i < configs.length; i++) {
+            RateLimiter.TokenBucketConfig memory config = configs[i];
+            tokenBucket[config.bucketId]._configTokenBucket(configs[i]);
+        }
     }
-    return _tokenBuckets;
-  }
 
-  function getCurrentSwapTokenBucketState(
-    address token,
-    string memory fromChainId
-  ) public view returns (RateLimiter.TokenBucket memory) {
-    bytes32 swapId = IBridgeOut(bridgeOut).getSwapId(token, fromChainId);
-    return tokenBucket[swapId]._currentTokenBucketState();
-  }
+    function getCurrentReceiptTokenBucketState(
+        address token,
+        string memory targetChainId
+    ) public view returns (RateLimiter.TokenBucket memory) {
+        bytes32 bucketId = _generateTokenKey(token, targetChainId);
+        return tokenBucket[bucketId]._currentTokenBucketState();
+    }
+
+    function getCurrentReceiptTokenBucketStates(
+        address[] memory tokens,
+        string[] memory targetChainIds
+    ) public view returns (RateLimiter.TokenBucket[] memory _tokenBuckets) {
+        _tokenBuckets = new RateLimiter.TokenBucket[](tokens.length);
+        for (uint i = 0; i < tokens.length; i++) {
+            bytes32 bucketId = _generateTokenKey(tokens[i], targetChainIds[i]);
+            _tokenBuckets[i] = tokenBucket[bucketId]._currentTokenBucketState();
+        }
+        return _tokenBuckets;
+    }
+
+    function getCurrentSwapTokenBucketState(
+        address token,
+        string memory fromChainId
+    ) public view returns (RateLimiter.TokenBucket memory) {
+        bytes32 swapId = IBridgeOut(bridgeOut).getSwapId(token, fromChainId);
+        return tokenBucket[swapId]._currentTokenBucketState();
+    }
 
     function getCurrentSwapTokenBucketStates(
-    address[] memory tokens,
-    string[] memory fromChainIds
-  ) public view returns (RateLimiter.TokenBucket[] memory _tokenBuckets) {
-    _tokenBuckets = new RateLimiter.TokenBucket[](tokens.length);
-    for (uint i = 0; i < tokens.length; i++) {
-      bytes32 swapId = IBridgeOut(bridgeOut).getSwapId(tokens[i], fromChainIds[i]);
-      _tokenBuckets[i] = tokenBucket[swapId]._currentTokenBucketState();
+        address[] memory tokens,
+        string[] memory fromChainIds
+    ) public view returns (RateLimiter.TokenBucket[] memory _tokenBuckets) {
+        _tokenBuckets = new RateLimiter.TokenBucket[](tokens.length);
+        for (uint i = 0; i < tokens.length; i++) {
+            bytes32 swapId = IBridgeOut(bridgeOut).getSwapId(tokens[i], fromChainIds[i]);
+            _tokenBuckets[i] = tokenBucket[swapId]._currentTokenBucketState();
+        }
+        return _tokenBuckets;
     }
-    return _tokenBuckets;
-  }
 
-  function getReceiptBucketMinWaitSeconds(
-    uint256 amount,
-    address token,
-    string memory targetChainId
-  ) public view returns (uint256) {
-    bytes32 bucketId = _generateTokenKey(token, targetChainId);
-    return getMinWaitSeconds(bucketId,amount);
-  }
-
-  function getSwapBucketMinWaitSeconds(
-    uint256 amount,
-    address token,
-    string memory fromChainId
-  ) public view returns (uint256) {
-    bytes32 swapId = IBridgeOut(bridgeOut).getSwapId(token, fromChainId);
-    return getMinWaitSeconds(swapId,amount);
-  }
-
-  function getMinWaitSeconds(bytes32 bucketId,uint256 amount) private view returns (uint256) {
-     RateLimiter.TokenBucket memory bucket = tokenBucket[bucketId]._currentTokenBucketState();
-    if (amount > bucket.currentTokenAmount) {
-      return
-        ((amount.sub(bucket.currentTokenAmount)).add(((uint256)(bucket.rate).sub(1)))).div(
-          bucket.rate
-        );
-    } else {
-      return 0;
+    function getReceiptBucketMinWaitSeconds(
+        uint256 amount,
+        address token,
+        string memory targetChainId
+    ) public view returns (uint256) {
+        bytes32 bucketId = _generateTokenKey(token, targetChainId);
+        return getMinWaitSeconds(bucketId, amount);
     }
-  }
 
-  function _generateTokenKey(
-    address token,
-    string memory chainId
-  ) private pure returns (bytes32) {
-    return sha256(abi.encodePacked(token, chainId));
-  }
+    function getSwapBucketMinWaitSeconds(
+        uint256 amount,
+        address token,
+        string memory fromChainId
+    ) public view returns (uint256) {
+        bytes32 swapId = IBridgeOut(bridgeOut).getSwapId(token, fromChainId);
+        return getMinWaitSeconds(swapId, amount);
+    }
+
+    function getMinWaitSeconds(bytes32 bucketId, uint256 amount) private view returns (uint256) {
+        RateLimiter.TokenBucket memory bucket = tokenBucket[bucketId]._currentTokenBucketState();
+        if (amount > bucket.currentTokenAmount) {
+            return
+                ((amount.sub(bucket.currentTokenAmount)).add(((uint256)(bucket.rate).sub(1)))).div(
+                bucket.rate
+            );
+        } else {
+            return 0;
+        }
+    }
+
+    function _generateTokenKey(
+        address token,
+        string memory chainId
+    ) private pure returns (bytes32) {
+        return sha256(abi.encodePacked(token, chainId));
+    }
 }
--- contracts/libraries/RateLimiter.sol
+++ contracts/libraries/RateLimiter.sol
@@ -1,120 +1,128 @@
 pragma solidity 0.8.9;
+
 import '@openzeppelin/contracts/utils/math/SafeMath.sol';
 
 library RateLimiter {
-  using SafeMath for uint256;
+    using SafeMath for uint256;
 
-  error BucketOverfilled();
-  error TokenRateLimitReached(uint256 minWaitInSeconds, uint256 available, address tokenAddress);
-  error MaxCapacityExceeded(uint256 capacity, uint256 amount);
-  event TokensConsumed(address tokenAddress, uint256 amount);
-  event ConfigChanged(TokenBucketConfig config);
+    error BucketOverfilled();
+    error TokenRateLimitReached(uint256 minWaitInSeconds, uint256 available, address tokenAddress);
+    error MaxCapacityExceeded(uint256 capacity, uint256 amount);
 
-  struct TokenBucket {
-    uint128 currentTokenAmount;
-    uint32 lastUpdatedTime;
-    bool isEnabled;
-    uint128 tokenCapacity;
-    uint128 rate;
-  }
+    event ConfigChanged(bytes32 bucketId, bool isEnabled, uint128 tokenCapacity, uint128 rate,
+        uint128 currentTokenAmount
+    );
 
-  struct TokenBucketConfig {
-    bytes32 bucketId; //tokenKey/swapId
-    bool isEnabled;
-    uint128 tokenCapacity;
-    uint128 rate;
-  }
-
-  function _consume(
-    TokenBucket storage _tokenBucket,
-    address _tokenAddress,
-    uint256 _amount
-  ) internal {
-    if (!_tokenBucket.isEnabled) {
-      return;
+    struct TokenBucket {
+        uint128 currentTokenAmount;
+        uint32 lastUpdatedTime;
+        bool isEnabled;
+        uint128 tokenCapacity;
+        uint128 rate;
     }
-    uint256 currentTokenAmount = _tokenBucket.currentTokenAmount;
-    uint256 capacity = _tokenBucket.tokenCapacity;
-    uint256 timeDiff = block.timestamp.sub(_tokenBucket.lastUpdatedTime);
-    if (timeDiff != 0) {
-      if (currentTokenAmount > capacity) revert BucketOverfilled();
-      currentTokenAmount = _calculateTokenRefill(
-        capacity,
-        currentTokenAmount,
-        _tokenBucket.rate,
-        timeDiff
-      );
-      _tokenBucket.lastUpdatedTime = uint32(block.timestamp);
-    }
-    if (capacity < _amount) {
-      revert MaxCapacityExceeded(capacity, _amount);
-    }
-    if (currentTokenAmount < _amount) {
-      uint256 rate = _tokenBucket.rate;
-      uint256 minWaitInSeconds = ((_amount.sub(currentTokenAmount)).add(rate.sub(1))).div(rate);
-      revert TokenRateLimitReached(minWaitInSeconds, _amount, _tokenAddress);
-    }
-    currentTokenAmount = currentTokenAmount.sub(_amount);
 
-    _tokenBucket.currentTokenAmount = uint128(currentTokenAmount);
-    emit TokensConsumed(_tokenAddress, _amount);
-  }
+    struct TokenBucketConfig {
+        bytes32 bucketId; //tokenKey/swapId
+        bool isEnabled;
+        uint128 tokenCapacity;
+        uint128 rate;
+    }
 
-  function _configTokenBucket(
-    TokenBucket storage _tokenBucket,
-    TokenBucketConfig memory _config
-  ) internal {
-    if (_tokenBucket.lastUpdatedTime != 0) {
-      uint256 timeDiff = block.timestamp.sub(_tokenBucket.lastUpdatedTime);
-      if (timeDiff != 0) {
+    function _consume(
+        TokenBucket storage _tokenBucket,
+        address _tokenAddress,
+        uint256 _amount
+    ) internal {
+        if (!_tokenBucket.isEnabled) {
+            return;
+        }
+        uint256 currentTokenAmount = _tokenBucket.currentTokenAmount;
+        uint256 capacity = _tokenBucket.tokenCapacity;
+        uint256 timeDiff = block.timestamp.sub(_tokenBucket.lastUpdatedTime);
+        if (timeDiff != 0) {
+            if (currentTokenAmount > capacity) revert BucketOverfilled();
+            currentTokenAmount = _calculateTokenRefill(
+                capacity,
+                currentTokenAmount,
+                _tokenBucket.rate,
+                timeDiff
+            );
+            _tokenBucket.lastUpdatedTime = uint32(block.timestamp);
+        }
+        if (capacity < _amount) {
+            revert MaxCapacityExceeded(capacity, _amount);
+        }
+        if (currentTokenAmount < _amount) {
+            uint256 rate = _tokenBucket.rate;
+            uint256 minWaitInSeconds = ((_amount.sub(currentTokenAmount)).add(rate.sub(1))).div(rate);
+            revert TokenRateLimitReached(minWaitInSeconds, _amount, _tokenAddress);
+        }
+        currentTokenAmount = currentTokenAmount.sub(_amount);
+
+        _tokenBucket.currentTokenAmount = uint128(currentTokenAmount);
+    }
+
+    function _configTokenBucket(
+        TokenBucket storage _tokenBucket,
+        TokenBucketConfig memory _config
+    ) internal {
+        if (_tokenBucket.lastUpdatedTime != 0) {
+            uint256 timeDiff = block.timestamp.sub(_tokenBucket.lastUpdatedTime);
+            if (timeDiff != 0) {
+                _tokenBucket.currentTokenAmount = uint128(
+                    _calculateTokenRefill(
+                        _tokenBucket.tokenCapacity,
+                        _tokenBucket.currentTokenAmount,
+                        _tokenBucket.rate,
+                        timeDiff
+                    )
+                );
+                _tokenBucket.lastUpdatedTime = uint32(block.timestamp);
+            }
+            _tokenBucket.currentTokenAmount = uint128(
+                _min(_config.tokenCapacity, _tokenBucket.currentTokenAmount)
+            );
+        } else {
+            _tokenBucket.currentTokenAmount = _config.tokenCapacity;
+            _tokenBucket.lastUpdatedTime = uint32(block.timestamp);
+        }
+        _tokenBucket.tokenCapacity = _config.tokenCapacity;
+        _tokenBucket.isEnabled = _config.isEnabled;
+        _tokenBucket.rate = _config.rate;
+        emit ConfigChanged(
+            _config.bucketId,
+            _config.isEnabled,
+            _config.tokenCapacity,
+            _config.rate,
+            _tokenBucket.currentTokenAmount
+        );
+    }
+
+    function _currentTokenBucketState(
+        TokenBucket memory _tokenBucket
+    ) internal view returns (TokenBucket memory) {
         _tokenBucket.currentTokenAmount = uint128(
-          _calculateTokenRefill(
-            _tokenBucket.tokenCapacity,
-            _tokenBucket.currentTokenAmount,
-            _tokenBucket.rate,
-            timeDiff
-          )
+            _calculateTokenRefill(
+                _tokenBucket.tokenCapacity,
+                _tokenBucket.currentTokenAmount,
+                _tokenBucket.rate,
+                block.timestamp.sub(_tokenBucket.lastUpdatedTime)
+            )
         );
         _tokenBucket.lastUpdatedTime = uint32(block.timestamp);
-      }
-      _tokenBucket.currentTokenAmount = uint128(
-        _min(_config.tokenCapacity, _tokenBucket.currentTokenAmount)
-      );
-    } else {
-      _tokenBucket.currentTokenAmount = _config.tokenCapacity;
-      _tokenBucket.lastUpdatedTime = uint32(block.timestamp);
+        return _tokenBucket;
     }
-    _tokenBucket.tokenCapacity = _config.tokenCapacity;
-    _tokenBucket.isEnabled = _config.isEnabled;
-    _tokenBucket.rate = _config.rate;
-    emit ConfigChanged(_config);
-  }
 
-  function _currentTokenBucketState(
-    TokenBucket memory _tokenBucket
-  ) internal view returns (TokenBucket memory) {
-    _tokenBucket.currentTokenAmount = uint128(
-      _calculateTokenRefill(
-        _tokenBucket.tokenCapacity,
-        _tokenBucket.currentTokenAmount,
-        _tokenBucket.rate,
-        block.timestamp.sub(_tokenBucket.lastUpdatedTime)
-      )
-    );
-    _tokenBucket.lastUpdatedTime = uint32(block.timestamp);
-    return _tokenBucket;
-  }
+    function _calculateTokenRefill(
+        uint256 capacity,
+        uint256 currentTokenAmount,
+        uint256 rate,
+        uint256 timeDiff
+    ) private pure returns (uint256) {
+        return _min(capacity, currentTokenAmount.add(timeDiff.mul(rate)));
+    }
 
-  function _calculateTokenRefill(
-    uint256 capacity,
-    uint256 currentTokenAmount,
-    uint256 rate,
-    uint256 timeDiff
-  ) private pure returns (uint256) {
-    return _min(capacity, currentTokenAmount.add(timeDiff.mul(rate)));
-  }
-
-  function _min(uint256 a, uint256 b) internal pure returns (uint256) {
-    return a < b ? a : b;
-  }
+    function _min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
 }
